5599a0c1e6016d5ae0c4a2415ebd49bdb2bc8754
ismartNishant <nishantrajputa11@gmail.com>
2022-03-20 12:22:55 +0530

    login setup completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,42 +1,46 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import $ from 'jquery';
+import { Movie } from "./Movie";
+import Unauth from "./Unauth";
 
 var newToken = createContext();
-
 const Login = (props) => {
     const [name, setName] = useState("");
     const [pswd, setPswd] = useState("");
-    const [UAuth,setUAuth] = useState("/uauth");
+    const [UAuth, setUAuth] = useState(false);
+    const [success, setSucsess] = useState(false);
 
-    const logBtnEnabled = () =>{
-       
-       var a = $("#iName").val().length;
-       var b = $("#iPass").val().length;
-        if  (a !== 0 && b !== 0) {
+    const logBtnEnabled = () => {
+
+        var a = $("#iName").val().length;
+        var b = $("#iPass").val().length;
+        if (a !== 0 && b !== 0) {
             $("#logBtn").removeClass("disabled");
-        
-        }else{
+
+        } else {
             $("#logBtn").addClass("disabled");
         }
 
     }
 
+
     const handleOnChange = (event) => {
         setName(event.target.value);
         logBtnEnabled();
-       
+
     };
     const handlePswd = (even) => {
         setPswd(even.target.value);
         logBtnEnabled();
     };
 
-   
+
 
     var tok;
-    var success;
+
     const handleLogin = (e) => {
+        e.preventDefault();
         $("#logBtn").addClass("disabled");
         fetch(" https://demo.credy.in/api/v1/usermodule/login/", {
             method: "POST",
@@ -49,20 +53,18 @@ const Login = (props) => {
             .then(res => res.json())
             .then(result => {
                 console.log(result.data.token);
-                success = result.is_success;
+                setSucsess(result.is_success);
                 tok = result.data.token;
-                if (success) {
+                if (result.is_success) {
                     $("#logBtn").removeClass("disabled");
                     localStorage.setItem('token', tok);
                     getToken();
-                    setUAuth("/movies")
                 }
-                else {
-                    alert("P;ease provide right");
-                }
+
                 console.log(tok);
                 console.log(success)
-
+            }).catch(error => {
+                alert("login failed... please provide right information")
             });
         //    var logBtn = document.getElementById('logBtn');
         //    $("input").onChange();
@@ -80,28 +82,34 @@ const Login = (props) => {
     let forbg = props.mode ? "#0000008a" : "#ffffff21";
     let forColor = props.mode ? "#000" : "#fff";
     let forShad = props.mode ? "0px 0px 10px #fff" : "0px 0px 10px #000";
-   
-    return (
-        <div className="main-login" >
-            <div className="login" style={{ background: forbg }}>
-                <form>
-                    <label className="login-label" htmlFor="chk" aria-hidden="true">
-                        Login
-                    </label>
-                    <input id= "iName" type="text" name="txt" placeholder="User Name" value={name} onChange={handleOnChange} required style={{ borderColor: forColor, boxShadow: forShad }} />
-                    <input id= "iPass" style={{ borderColor: forColor, boxShadow: forShad }}  type="text"   name="pswd"   placeholder="Password"
-                        value={pswd}
-                        onChange={handlePswd}
-                        required
-                    />
 
-                    <button id="logBtn" className="btn disabled mybtn" onClick={handleLogin}>
-                        <Link  to="/movie">login</Link>
-                    </button>
-                </form>
+    if (!success) {
+        return (
+            <div className="main-login" >
+                <div className="login" style={{ background: forbg }}>
+                    <form>
+                        <label className="login-label" htmlFor="chk" aria-hidden="true">
+                            Login
+                        </label>
+                        <input id="iName" type="text" name="txt" placeholder="User Name" value={name} onChange={handleOnChange} required style={{ borderColor: forColor, boxShadow: forShad }} />
+                        <input id="iPass" style={{ borderColor: forColor, boxShadow: forShad }} type="text" name="pswd" placeholder="Password"
+                            value={pswd}
+                            onChange={handlePswd}
+                            required
+                        />
+                        <button id="logBtn" className="btn disabled mybtn" onClick={handleLogin}>
+                            login
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else if (success) {
+        return (
+            <Movie />
+        );
+    }
 };
 
 export default Login;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState  } from 'react'
 import { newToken } from './Login';
 import { Link } from "react-router-dom";
 
@@ -6,37 +6,38 @@ import { Link } from "react-router-dom";
 export const Movie = (props) => {
     const [mList, setMlist] = useState([]);
     const [page, setPage] = useState(1);
-    var movieListStatic = [
-        { title: 'Queerama', description: '50 years after decriminalisation of homosexuality …essions of gay men and women in the 20th century.', genres: '', uuid: '57baf4f4-c9ef-4197-9e4f-acf04eae5b4d' },
-        { title: 'Satana likuyushchiy', description: "In a small town live two brothers, one a minister …born from the minister's adulterous relationship.", genres: '', uuid: '163ce013-03e2-47e9-8afd-e7de7688c151' },
-        { title: 'Betrayal', description: 'When one of her hits goes wrong, a professional as… of a million dollars belonging to a mob boss ...', genres: 'Action,Drama,Thriller', uuid: '720e8796-5397-4e81-9bd7-763789463707' },
-        { title: 'Siglo ng Pagluluwal', description: 'An artist struggles to finish his work while a storyline about a cult plays in his head.', genres: 'Drama', uuid: 'e9548ee7-6a95-4917-893e-1fa1d3a6de40' },
-        { title: 'رگ خواب', description: 'Rising and falling between a man and woman.', genres: 'Drama,Family', uuid: '9b0a4aa2-9ec7-4a3d-98ab-622275f44ea5' }
-    ];
+   
     var movielenght = undefined;
-    const serachMovie = () => {
-        var val = newToken;
-        fetch(`https://demo.credy.in/api/v1/maya/movies/?page=2`, {
-            method: "GET",
 
-            headers: new Headers({
-                'Authorization': 'Token ' + val,
+    const serachMovie = async () => {
+        var val = newToken;
+        try {
+            let data = await  fetch(`https://demo.credy.in/api/v1/maya/movies/?page=2`, {
+                method: "GET",
+    
+                headers: new Headers({
+                    'Authorization': 'Token ' + val,
+                })
             })
-        })
-            .then(res => res.json())
-            .then(result => {
-                console.log(result);
-                setMlist(result.results)
-                console.log(mList)
-                movielenght = mList.length
-                console.log(mList[3])
-                // setPage(page + 1)
-            });
-        console.log(val + "in movie ");
+            let parsedData = await data.json();
+    
+                    console.log(parsedData);
+                    setMlist(parsedData.results)
+                    console.log(mList)
+                    movielenght = mList.length
+                    console.log(mList[3])
+                    // setPage(page + 1)
+            console.log(val + "in movie ");
+        } catch (error) {
+            alert("eroooooooooooorrrrrrrrrrrrrrrrrrr");
+        }
     }
 
+    
+
+
     let forbg = props.mode ? "#fff" : "#000";
-    let forColor = props.mode ? "#000" : "#fff";
+    let forColor = props.mode ? "dark" : "light";
     let forShad = props.mode ? "0px 0px 10px #f7f1f19e" : "0px 0px 10px #0000009e";
 
     var element;
@@ -55,32 +56,23 @@ export const Movie = (props) => {
     return (
         <div className='movie'>
 
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <h2 className="navbar-brand" href="#">Cinema</h2>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link active" to="/">Home</Link>
-                            </li>
-                        </ul>
-
-                    </div>
+            <nav className={`navbar m-0 navbar-${forColor} bg-${forColor} fixed-top`} style={{boxShadow:forShad}}>
+                <div className="">
+                    <h2 className="p-0 m-0 navbar-brand" >Cinema<i class='bx bxs-movie-play bx-tada' ></i></h2>
                 </div>
+                <button className='btn btn-outline-danger' onClick={serachMovie}>Search-Movie</button>  
             </nav>
-            <div className='search-btn'>
-                <button className='btn btn-outline-danger' onClick={serachMovie}>Click Me</button>
-            </div>
+            <div className='movie-btn'>
+             <button className='btn btn-danger' onClick={serachMovie}>Previous</button>
+             <button className='btn btn-danger' onClick={serachMovie}>Next</button>
+           </div>
             <div className='in-box-1'>
                 {mList.map((element) => {
                     return (
-
-                        <div class="in-box-2">
-                            <div class="cards"  >
-                                <div class="front">
+                         
+                        <div className="in-box-2">
+                            <div className="cards"  >
+                                <div className="front">
                                     <div className='inner-movie h-100 card p-0 m-0' key={element.uuid} style={{ borderColor: forbg, boxShadow: forShad, color: forbg }}>
                                         <img src={`https://ui-avatars.com/api/?rounded=true&background=random&name=${element.title}`} alt='..' />
                                         <div className="card-body">
@@ -92,7 +84,7 @@ export const Movie = (props) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="back">
+                                <div className="back">
                                     <div className='inner-movie h-100 card p-0 m-0' key={element.uuid} style={{ borderColor: forbg, boxShadow: forShad, color: forbg }}>
                                         <img src={`https://ui-avatars.com/api/?background=random&name=${element.title}`} alt='..' />
                                         <div className="card-body">
@@ -120,6 +112,7 @@ export const Movie = (props) => {
                     );
                 })}
             </div>
+            
         </div>
 
     )
